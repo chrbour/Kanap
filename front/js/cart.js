005a68d512c4d4fdbf513ca7947c3ab3f0be1018
chrbour <108238838+chrbour@users.noreply.github.com>
2023-04-02 22:21:19 +0200

    Problème: addEventListener se met sur un seul élément

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,25 +1,48 @@
 let objLinea=localStorage.getItem("canapes");
 let objJson=JSON.parse(objLinea);
 const nom=document.getElementById("cart__items");
+const modifQtite=document.getElementsByClassName('itemQuantity');
+const prenom=document.getElementById('firstName');
 let sectionContent="";
+let quantiteTotal=0;
+let prixTotal=0;
+let selecteurs=[];
+let i=0;
 
-// Displays the selected products
-for (let canape of objJson){console.log("id canape boucle: ",canape.id);
+// Displays the selected products with elements form localStorage and API
+for (let canape of objJson){/*console.log("id canape boucle: ",canape.id);*/
     fetch(`http://localhost:3000/api/products/${canape.id}`)
     .then ((res)=>{
         if (res.ok){
             return res.json()
         }
     })
-    .then ((value)=>{console.log("valeur: ",value);
-        /*console.log("id canape: ",canape.id," objJson:",objJson);*/
-        affichage(value);
+    .then ((value)=>{/*console.log("valeur: ",value);*/
+        // Affichage des éléments sélectionnés dans le panier, 
+        // calcul du nombre d'éléments et du prix total
+        affichage(value); 
+        document.getElementById("totalQuantity").innerHTML=quantiteTotal;/*console.log(quantiteTotal);*/
+        document.getElementById("totalPrice").innerHTML=prixTotal;
+        
+        return modifQtite[i];
     })
     .catch((err)=>{
-        console.log(err)
+        console.log(err);
     });
+    
+/**
+ * Updates quantity of elements ine the cart, price and the localStorage
+ */
+const modification = () =>{
+    console.log('Essai OK');
+   }
+  
 
-    const affichage=(maSelection)=>{console.log("canape:",canape.id);
+/**
+ * Writes HTML for each product in the cart
+ * @param {maSelection} Objet
+ */
+    const affichage=(maSelection)=>{
         sectionContent=sectionContent+
         `<article class="cart__item" data-id="${canape.id}" data-color="${canape.couleur}">
             <div class="cart__item__img">
@@ -34,7 +57,8 @@ for (let canape of objJson){console.log("id canape boucle: ",canape.id);
                 <div class="cart__item__content__settings">
                     <div class="cart__item__content__settings__quantity">
                         <p>Quantité: </p>
-                        <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value=${canape.quantite}>
+                        <input type="number" class="itemQuantity" name="itemQuantity" 
+                            min="1" max="100" value=${canape.quantite}>
                     </div>
                     <div class="cart__item__content__settings__delete">
                         <p class="deleteItem">Supprimer</p>
@@ -43,5 +67,12 @@ for (let canape of objJson){console.log("id canape boucle: ",canape.id);
             </div>    
         </article>`;
         nom.innerHTML=sectionContent;
+        quantiteTotal+=Number(canape.quantite);
+        prixTotal+=Number(canape.quantite)*Number(maSelection.price);
+        // Allows quantity to be modified
+        selecteurs.push(modifQtite[i]);
+        modifQtite[i].addEventListener('change',modification);
+        console.log("selecteurs:",selecteurs);
+        i++;
     }
 }
